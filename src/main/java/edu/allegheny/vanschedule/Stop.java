aa81bc1e7e7b09a5410f4f3161ba85d70b9d35a6
Cody Kinneer <kinneerc@allegheny.edu>
2015-11-20 09:34:54 -0500

    started Stop class

--- src/main/java/edu/allegheny/vanschedule/Stop.java
+++ src/main/java/edu/allegheny/vanschedule/Stop.java
@@ -1,5 +1,28 @@
 package edu.allegheny.vanschedule;
 
-public class Stop {
+public class Stop extends Place {
+
+    private Time arrival;
+    private Time departure;
+
+    public Stop(Place p){
+        super(p.getName());
+    }
+
+    public void setArrival(Time arrival){
+        this.arrival = arrival;
+    }
+
+    public void setDeparture(Time departure){
+        this.departure = departure;
+    }
+
+    public Time getArrival(){
+        return arrival;
+    }
+
+    public Time getDeparture(){
+        return departure;
+    }
 
 }
