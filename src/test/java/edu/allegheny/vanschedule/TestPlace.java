47fde7138da8a7c898d50bc4791288a250678482
Cody Kinneer <kinneerc@allegheny.edu>
2015-11-20 09:35:39 -0500

    started TestPlace

---
+++ src/test/java/edu/allegheny/vanschedule/TestPlace.java
@@ -0,0 +1,24 @@
+package edu.allegheny.vanschedule;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestPlace {
+
+    @Test
+    public void testPlace(){
+    }
+
+    @Test
+    public void testGetName(){
+    }
+
+    @Test 
+    public void testGetAddress(){
+    }
+
+    @Test 
+    public void testSetAddress(){
+    }
+
+}
