f44bf76dfbd1c257c290dabd0fc23e786d652694
Cody Kinneer <kinneerc@allegheny.edu>
2015-11-20 09:35:50 -0500

    started TestStop

---
+++ src/test/java/edu/allegheny/vanschedule/TestStop.java
@@ -0,0 +1,29 @@
+package edu.allegheny.vanschedule;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestStop {
+
+    @Test
+    public void testStop(){
+    }
+
+    @Test
+    public void testGetArrival(){
+    }
+
+    @Test 
+    public void testGetDeparture(){
+    }
+
+    @Test 
+    public void testSetArrival(){
+    }
+
+    @Test 
+    public void testSetDeparture(){
+    }
+
+
+}
