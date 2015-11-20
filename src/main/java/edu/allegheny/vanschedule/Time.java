b50a8fffa38ba397768e47aa3d8ca2b6a118e895
Cody Kinneer <kinneerc@allegheny.edu>
2015-11-20 10:05:51 -0500

    started Time representation

--- src/main/java/edu/allegheny/vanschedule/Time.java
+++ src/main/java/edu/allegheny/vanschedule/Time.java
@@ -1,5 +1,46 @@
 package edu.allegheny.vanschedule;
 
+/**
+ * Time representation
+ * @author Cody Kinneer
+ */
 public class Time {
+
+    private int hour;
+    private int minuite;
+
+    public Time (int hour, int minuite){
+        this.hour = hour;
+        this.minuite = minuite;
+    }
+
+    public int getHour(){
+        return hour;
+    }
+
+    public int getMinuite(){
+        return minuite;
+    }
+
+    public void setHour(int hour){
+        if (hour >= 0 && hour <= 23)
+            this.hour = hour;
+    }
+
+    public void setMinuite(int minuite){
+        if (minuite >= 0 && minuite <= 59)
+            this.minuite = minuite;
+    }
+
+    //TODO make this work
+
+    /**
+     * Returns the differance from another Time in miniutes
+     * @param time the time to compute differance from
+     * @return the differance between this time and the input time in minuites
+     */
+    public int getDiff(Time time){
+        return 0;
+    }
 
 }
