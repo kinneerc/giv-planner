cbb0d5bf92fee49b27257e588ed275af83dd28c3
Cody Kinneer <kinneerc@allegheny.edu>
2015-11-20 09:34:39 -0500

    started Place class

--- src/main/java/edu/allegheny/vanschedule/Place.java
+++ src/main/java/edu/allegheny/vanschedule/Place.java
@@ -2,4 +2,23 @@ package edu.allegheny.vanschedule;
 
 public class Place {
 
+    private String name;
+    private String address;
+
+    public Place(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public String setAddress(String address){
+        this.address = address;
+    }
+
 }
