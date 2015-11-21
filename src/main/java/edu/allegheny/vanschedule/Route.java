fd9d7cf016860cd2a65b6a8b1557e4151299f410
Cody Kinneer <kinneerc@allegheny.edu>
2015-11-21 17:30:34 -0500

    fixed null list

--- src/main/java/edu/allegheny/vanschedule/Route.java
+++ src/main/java/edu/allegheny/vanschedule/Route.java
@@ -1,10 +1,15 @@
 package edu.allegheny.vanschedule;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Route {
 	
 private List<Stop> stops;
+
+public Route(){
+	stops = new ArrayList<Stop>();
+}
 
 /**
  * @return the stops
@@ -18,6 +23,10 @@ public List<Stop> getStops() {
  */
 public void setStops(List<Stop> stops) {
 	this.stops = stops;
+}
+
+public void addStop(Stop stop){
+	stops.add(stop);
 }
 
 }
