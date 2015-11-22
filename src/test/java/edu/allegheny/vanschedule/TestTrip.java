06ee7925f41978e7ee0193a6efb589439d7679ab
Colton <mccurdyc@allegheny.edu>
2015-11-22 12:05:57 -0500

    Adding a class for testing Trip.java

---
+++ src/test/java/edu/allegheny/vanschedule/TestTrip.java
@@ -0,0 +1,32 @@
+package edu.allegheny.vanschedule;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestTrip {
+
+    @Test
+    public void testGetStart(){
+      Place s = new Place("Here");
+      Place e = new Place("There");
+      Trip t = new Trip(s,e);
+
+      Place actual = t.getStart();
+
+      assertEquals(s.getName(), actual.getStart());
+
+    }
+
+    @Test
+    public void testSetStart(){
+      Place s = new Place("Here");
+      Place e = new Place("There");
+      Trip t = new Trip(s,e);
+
+      t.setStart(s);
+      Place actual = t.getStart();
+
+      assertEquals(s.getName(), actual.getStart());
+
+    }
+}
