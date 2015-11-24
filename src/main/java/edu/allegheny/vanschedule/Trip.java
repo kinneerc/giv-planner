3fe6d8ab975c8e4e2e7ac2114ae074b4d3c268b2
Cody Kinneer <kinneerc@allegheny.edu>
2015-11-24 11:20:20 -0500

    added Trip api

--- src/main/java/edu/allegheny/vanschedule/Trip.java
+++ src/main/java/edu/allegheny/vanschedule/Trip.java
@@ -1,10 +1,18 @@
 package edu.allegheny.vanschedule;
 
+import java.util.List;
+
 public class Trip {
 	
+	private static List<Trip> allTrips;
+
 	private Place start;
 	private Place end;
 	
+	public double getTravelTime(){
+	    return Double.MAX_VALUE;
+    }
+
 	public Trip(Place start, Place end){
 		this.start = start;
 		this.end = end;
@@ -23,6 +31,12 @@ public class Trip {
 	public void setStart(Place start) {
 		this.start = start;
 	}
+
+	public static List<Trip> getTrips(){
+	    return allTrips;
+    }
+
+
 
 
 }
