42d05bb376fdec56029ff495c4f52c198f8740d2
Cody Kinneer <kinneerc@allegheny.edu>
2015-12-03 15:21:50 -0500

    added full featured time getting functionality

--- src/main/java/edu/allegheny/vanschedule/frontend/GoogleMaps.java
+++ src/main/java/edu/allegheny/vanschedule/frontend/GoogleMaps.java
@@ -1,8 +1,12 @@
 package edu.allegheny.vanschedule.frontend;
 
 
+import java.util.List;
+
 import com.google.maps.*;
 import com.google.maps.model.*;
+
+import edu.allegheny.vanschedule.Place;
 
 /**
  * This class is in charge of getting time information from google maps for the stops that we want
@@ -13,16 +17,58 @@ public class GoogleMaps {
     private static String apiKey = "AIzaSyBiplH2PqwslW7IPn9eaWPOftwIJnJgMuM";
 
     private GeoApiContext context;
+    
+    private List<Place> places;
+    
+    private DistanceMatrix ans;
 
-    public GoogleMaps(){
+    public GoogleMaps(List<Place> places) throws Exception{
         context = new GeoApiContext().setApiKey(apiKey);
+        this.places = places;
+        
+        ans = callGMaps(getPlaceList());
+        
+    }
+    
+    /**
+     * Get the travel time between two places
+     * @param source Place
+     * @param dest Place
+     * @return the number of minutes to travel from source to dest
+     */
+    public double getTravelTime(Place source, Place dest){
+    	
+    	int sourceI = places.indexOf(source);
+    	int destI = places.indexOf(dest);
+    	
+    	DistanceMatrixRow[] rows =  ans.rows;
+    	
+    	DistanceMatrixRow t = rows[sourceI];
+    	
+    	DistanceMatrixElement e = t.elements[destI];
+    	
+    	return e.duration.inSeconds / 60.0;
+    	
     }
 
-    public DistanceMatrixApiRequest callGMaps(String[] origins, String[] destinations){
-        return DistanceMatrixApi.getDistanceMatrix(context, origins, destinations); 
+    private String[] getPlaceList(){
+    	
+    	String[] placeString = new String[places.size()];
+    	
+    	for(int i = 0; i < places.size(); i++){
+    		placeString[i] = places.get(i).getName() + ", Meadville PA";
+    	}
+    	
+		return placeString;
+ 
+    }
+    
+    public DistanceMatrix callGMaps(String[] origins) throws Exception{
+        return DistanceMatrixApi.getDistanceMatrix(context, origins, origins).await(); 
     }
     
     // testing purposes only
+    /*
     public static void main(String[] args){
     	GoogleMaps gm = new GoogleMaps();
     	DistanceMatrixApiRequest req = gm.callGMaps(new String[] {"Allegheny College, Meadville PA"},new String[] {"Super S Buffet, Erie PA"});
@@ -33,7 +79,9 @@ public class GoogleMaps {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
     	
     }
+    */
 
 }
