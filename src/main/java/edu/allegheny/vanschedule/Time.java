bf0b8d644445a01c79187f0d983e4bfab53a47cd
Cody Kinneer <kinneerc@allegheny.edu>
2015-12-04 10:52:20 -0500

    refactored parseTime method to the Time class

--- src/main/java/edu/allegheny/vanschedule/Time.java
+++ src/main/java/edu/allegheny/vanschedule/Time.java
@@ -53,9 +53,63 @@ public class Time {
         return -minDiff;
     }
 
-    public static Time parse(String in){
-        return new Time(0,0);
-    }
+    /*
+	* parseTime()
+	* 
+	* @param String
+	* @return Time
+	*
+	* Take a string representation of a time. 
+	* Return it as a Time object.
+	 */
+	
+	public static Time parse(String t) {
+		String[] initial = t.split("\\s+");
+		String[] hourMinute = initial[0].split(":");
+		Time tempTime;
+
+		// check if null
+		// if so return -1,-1
+		if (t.equals("")) {
+			tempTime = new Time(-1,-1);
+			return tempTime;
+		}
+
+		// if it is not null
+		else {
+				// if it is am do not add 12 to time
+				if (initial[1].toUpperCase().equals("AM")) {
+					if (hourMinute[0].equals("12")) {
+						int hourAM = 0;
+						int minuteAM = Integer.parseInt(hourMinute[1]);
+						tempTime = new Time(hourAM,minuteAM);
+					} // inner if
+					else {
+						int hour = Integer.parseInt(hourMinute[0]);
+						int minute = Integer.parseInt(hourMinute[1]);
+						tempTime = new Time(hour,minute);
+					} // inner else
+				} // inner if
+
+				// if pm add 12 to time
+				else {
+					if (hourMinute[0].equals("12")) {
+						int hourPMT = Integer.parseInt(hourMinute[0]);
+						int minutePMT = Integer.parseInt(hourMinute[1]);
+						tempTime = new Time(hourPMT,minutePMT);
+					} // inner if
+					else {
+						int hourPM = Integer.parseInt(hourMinute[0]);
+						int minutePM = Integer.parseInt(hourMinute[1]);
+						hourPM = hourPM + 12;
+						tempTime = new Time(hourPM,minutePM);
+					} // inner else
+				} // inner else
+			} // big else (not null)
+			return tempTime;
+		
+	} // parseTime
+    
     
 	@Override
 	public String toString(){
--- src/main/java/edu/allegheny/vanschedule/frontend/DailyRequestMaker.java
+++ src/main/java/edu/allegheny/vanschedule/frontend/DailyRequestMaker.java
@@ -55,71 +55,14 @@ public class DailyRequestMaker {
 		//need to make the times and place into objects
 		String name = info[1];
 		String email = info[2];
-		Time arrive = parseTime(info[7]);
-		Time depart = parseTime(info[8]);
+		Time arrive = Time.parse(info[7]);
+		Time depart = Time.parse(info[8]);
 		Place site = new Place(info[4]);
 		Request request = new Request(site,arrive,depart,name,email);
 
 		return request;
 
 	} // makeRequest
-
-	/*
-	* parseTime()
-	* 
-	* @param String
-	* @return Time
-	*
-	* Take a string representation of a time. 
-	* Return it as a Time object.
-	 */
-	
-	public static Time parseTime(String t) {
-		String[] initial = t.split("\\s+");
-		String[] hourMinute = initial[0].split(":");
-		Time tempTime;
-
-		// check if null
-		// if so return -1,-1
-		if (t.equals("")) {
-			tempTime = new Time(-1,-1);
-			return tempTime;
-		}
-
-		// if it is not null
-		else {
-				// if it is am do not add 12 to time
-				if (initial[1].equals("AM")) {
-					if (hourMinute[0].equals("12")) {
-						int hourAM = 0;
-						int minuteAM = Integer.parseInt(hourMinute[1]);
-						tempTime = new Time(hourAM,minuteAM);
-					} // inner if
-					else {
-						int hour = Integer.parseInt(hourMinute[0]);
-						int minute = Integer.parseInt(hourMinute[1]);
-						tempTime = new Time(hour,minute);
-					} // inner else
-				} // inner if
-
-				// if pm add 12 to time
-				else {
-					if (hourMinute[0].equals("12")) {
-						int hourPMT = Integer.parseInt(hourMinute[0]);
-						int minutePMT = Integer.parseInt(hourMinute[1]);
-						tempTime = new Time(hourPMT,minutePMT);
-					} // inner if
-					else {
-						int hourPM = Integer.parseInt(hourMinute[0]);
-						int minutePM = Integer.parseInt(hourMinute[1]);
-						hourPM = hourPM + 12;
-						tempTime = new Time(hourPM,minutePM);
-					} // inner else
-				} // inner else
-			} // big else (not null)
-			return tempTime;
-		
-	} // parseTime
 
 } // DailyRequestMaker
 
