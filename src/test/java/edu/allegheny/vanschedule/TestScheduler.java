b710be074476ecd8321b918b8e79ca9d7d21cc20
Colton <mccurdyc@allegheny.edu>
2015-12-05 10:13:32 -0500

    Removed trailing white space

--- src/test/java/edu/allegheny/vanschedule/TestScheduler.java
+++ src/test/java/edu/allegheny/vanschedule/TestScheduler.java
@@ -8,33 +8,33 @@ import java.util.Random;
 import static org.junit.Assert.assertEquals;
 
 public class TestScheduler {
-	
+
 	@Test
 	public void testSchedule(){
-	
+
 		ArrayList<Request> reqs = new ArrayList<Request>();
-		
+
 		Request req1 = new Request(new Place("Botswana"),new Time(1,30),new Time(4,30));
 		Request req2 = new Request(new Place("Iraq"),new Time(1,55),new Time(5,30));
 
 		reqs.add(req1); reqs.add(req2);
-		
+
 		Route ans = Scheduler.schedule(reqs);
-		
+
 		assertEquals(true,req1.satisfied(ans));
 		assertEquals(true,req2.satisfied(ans));
-		
+
 	}
-	
+
 	@Test
 	public void testScheduleRand(){
-		
+
 		Random rand = new Random();
-		
-		for (int trials = 0; trials < 50; trials++){ 
-		
+
+		for (int trials = 0; trials < 50; trials++){
+
 		ArrayList<Request> reqs = new ArrayList<Request>();
-		
+
 		for (int reqCount = 0; reqCount < 12; reqCount++){
 			reqs.add(
 					new Request(
@@ -42,13 +42,11 @@ public class TestScheduler {
 							new Time(rand.nextInt(11),rand.nextInt(60)),
 							new Time(rand.nextInt(11)+12,rand.nextInt(60))));
 		}
-		
+
 		Route ans = Scheduler.schedule(reqs);
-		
+
 		for (Request r : reqs)
 			assertEquals(true,r.satisfied(ans));
-		
-	}
-	}
 
+	}
 }
