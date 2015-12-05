66a2913454f5018b27b4e00c0207a1d51993ce0a
Colton <mccurdyc@allegheny.edu>
2015-12-04 21:09:23 -0500

    Adding tests for Stop class. The unit tests for set and get Arrival and Departure are the same because otherwise I could not access arrival or departure

--- src/test/java/edu/allegheny/vanschedule/TestStop.java
+++ src/test/java/edu/allegheny/vanschedule/TestStop.java
@@ -7,23 +7,81 @@ public class TestStop {
 
     @Test
     public void testStop(){
+      Place p = new Place("Here");
+      Stop s = new Stop(p);
+
+      assertEquals("Here",s.getName());
+
     }
 
     @Test
     public void testGetArrival(){
+      Place p = new Place("Here");
+      Stop s = new Stop(p);
       Time t = new Time(10,20);
+      s.setArrival(t);
+
+      Time actual = s.getArrival();
+
+      assertEquals(actual.getHour(), t.getHour());
+      assertEquals(actual.getMinute(), t.getMinute());
     }
 
     @Test
     public void testGetDeparture(){
+      Place p = new Place("There");
+      Stop s = new Stop(p);
+      Time t = new Time(11,30);
+      s.setDeparture(t);
+
+      Time actual = s.getDeparture();
+
+      assertEquals(actual.getHour(), t.getHour());
+      assertEquals(actual.getMinute(), t.getMinute());
     }
 
     @Test
     public void testSetArrival(){
+      Place p = new Place("Here");
+      Stop s = new Stop(p);
+      Time t = new Time(10,20);
+      s.setArrival(t);
+
+      Time actual = s.getArrival();
+
+      assertEquals(actual.getHour(), t.getHour());
+      assertEquals(actual.getMinute(), t.getMinute());
     }
 
     @Test
     public void testSetDeparture(){
+      Place p = new Place("There");
+      Stop s = new Stop(p);
+      Time t = new Time(11,30);
+      s.setDeparture(t);
+
+      Time actual = s.getDeparture();
+
+      assertEquals(actual.getHour(), t.getHour());
+      assertEquals(actual.getMinute(), t.getMinute());
+    }
+
+    @Test
+    public void testToString(){
+      Place p = new Place("There");
+      Stop s = new Stop(p);
+      Time a = new Time(11,30);
+      Time d = new Time(13,30);
+      s.setArrival(a);
+      s.setDeparture(d);
+
+      Time arrival = s.getArrival();
+      Time depart = s.getDeparture();
+
+      /* Time actual = s.getDeparture(); */
+      String expected = arrival +" "+ p.getName() +" "+ depart;
+
+      assertEquals(expected, s.toString());
     }
 
 
