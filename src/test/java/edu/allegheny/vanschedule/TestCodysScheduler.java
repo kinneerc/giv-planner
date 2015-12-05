ab1d1cd84f447117adaf662405120fe0f0432f0a
Colton <mccurdyc@allegheny.edu>
2015-12-05 10:14:21 -0500

    Creating templates for test. Will fill these in tonight and finalize testing

---
+++ src/test/java/edu/allegheny/vanschedule/TestCodysScheduler.java
@@ -0,0 +1,45 @@
+package edu.allegheny.vanschedule;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestScheduler {
+
+	@Test
+	public void testSchedule(){
+	}
+
+	@Test
+	public void testFixSamePlaceSameTime(){
+	}
+
+	@Test
+	public void testRemoveStops(){
+	}
+
+	@Test
+	public void testBackTrips(){
+	}
+
+	@Test
+	public void testToRoute(){
+	}
+
+	@Test
+	public void testAddAlleghenyStartEnd(){
+	}
+
+	@Test
+	public void testInitializeMap(){
+	}
+
+	@Test
+	public void testGetStopRequests(){
+	}
+
+
+}
---
+++ src/test/java/edu/allegheny/vanschedule/TestStopRequest.java
@@ -0,0 +1,52 @@
+package edu.allegheny.vanschedule;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestStopRequest {
+
+    @Test
+    public void testGetStopRequests(){
+    }
+
+    @Test
+    public void testToStop(){
+    }
+
+    @Test
+    public void testCompareTo(){
+    }
+
+    @Test
+    public void testGetSite(){
+    }
+
+    @Test
+    public void testSetSite(){
+    }
+
+    @Test
+    public void testGetDesiredTime(){
+    }
+
+    @Test
+    public void testSetDesiredTime(){
+    }
+
+    @Test
+    public void testIsCanBeEarly(){
+    }
+
+    @Test
+    public void testSetCanBeEarly(){
+    }
+
+    @Test
+    public void testIsCanBeLate(){
+    }
+
+    @Test
+    public void testSetCanBeLate(){
+    }
+
+}
