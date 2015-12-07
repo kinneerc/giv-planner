3bf7f56337ba316cbc9a1850b9b075e9d9396587
Colton <mccurdyc@allegheny.edu>
2015-12-06 20:58:37 -0500

    Boxed zip download

--- src/test/java/edu/allegheny/vanschedule/TestCodysScheduler.java
+++ src/test/java/edu/allegheny/vanschedule/TestCodysScheduler.java
@@ -11,35 +11,79 @@ public class TestCodysScheduler {
 
 	@Test
 	public void testSchedule(){
-	}
+	
+		ArrayList<Request> reqs = new ArrayList<Request>();
+		
+		Request req1 = new Request(new Place("Botswana"),new Time(1,30),new Time(4,30));
+		Request req2 = new Request(new Place("Iraq"),new Time(1,55),new Time(5,30));
 
-	@Test
-	public void testFixSamePlaceSameTime(){
+		reqs.add(req1); reqs.add(req2);
+		
+		Route ans = Scheduler.schedule(reqs);
+		
+		assertEquals(true,req1.satisfied(ans));
+		assertEquals(true,req2.satisfied(ans));
+		
 	}
-
-	@Test
-	public void testRemoveStops(){
-	}
-
-	@Test
-	public void testBackTrips(){
-	}
-
-	@Test
-	public void testToRoute(){
-	}
-
-	@Test
-	public void testAddAlleghenyStartEnd(){
-	}
-
-	@Test
-	public void testInitializeMap(){
-	}
-
-	@Test
-	public void testGetStopRequests(){
-	}
+	
+/* 	@Test */
+/* 	public void testScheduleRand(){ */
+/* 		 */
+/* 		Random rand = new Random(); */
+/* 		 */
+/* 		for (int trials = 0; trials < 50; trials++){  */
+/* 		 */
+/* 		ArrayList<Request> reqs = new ArrayList<Request>(); */
+/* 		 */
+/* 		for (int reqCount = 0; reqCount < 12; reqCount++){ */
+/* 			reqs.add( */
+/* 					new Request( */
+/* 							new Place(rand.nextLong()+""), */
+/* 							new Time(rand.nextInt(11),rand.nextInt(60)), */
+/* 							new Time(rand.nextInt(11)+12,rand.nextInt(60)))); */
+/* 		} */
+/* 		 */
+/* 		Route ans = Scheduler.schedule(reqs); */
+/* 		 */
+/* 		for (Request r : reqs) */
+/* 			assertEquals(true,r.satisfied(ans)); */
+/* 		 */
+/* 	} */
+/* 	} */
+/*  */
+/* } */
+/*  */
+/* 	@Test */
+/* 	public void testSchedule(){ */
+/* 	} */
+/*  */
+/* 	@Test */
+/* 	public void testFixSamePlaceSameTime(){ */
+/* 	} */
+/*  */
+/* 	@Test */
+/* 	public void testRemoveStops(){ */
+/* 	} */
+/*  */
+/* 	@Test */
+/* 	public void testBackTrips(){ */
+/* 	} */
+/*  */
+/* 	@Test */
+/* 	public void testToRoute(){ */
+/* 	} */
+/*  */
+/* 	@Test */
+/* 	public void testAddAlleghenyStartEnd(){ */
+/* 	} */
+/*  */
+/* 	@Test */
+/* 	public void testInitializeMap(){ */
+/* 	} */
+/*  */
+/* 	@Test */
+/* 	public void testGetStopRequests(){ */
+/* 	} */
 
 
 }
--- src/test/java/edu/allegheny/vanschedule/TestStopRequest.java
+++ src/test/java/edu/allegheny/vanschedule/TestStopRequest.java
@@ -3,15 +3,52 @@ package edu.allegheny.vanschedule;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.util.*;
+
 public class TestStopRequest {
+    @Test
+    public void testStopRequests(){
+      Place p = new Place("Here");
+      Time t = new Time(10,55);
+      StopRequest sr = new StopRequest(p,t);
+    }
 
     @Test
     public void testGetStopRequests(){
+      Place p = new Place("Here");
+      Time a = new Time(10,55);
+      Time d = new Time(11,30);
+
+      Time t = new Time(10,55);
+      StopRequest sr = new StopRequest(p,t);
+
+      Request r = new Request(p,a,d);
+      List<StopRequest> actual = sr.getStopRequests(r);
+
+      StopRequest sOne = new StopRequest(r.getSite(),r.getDesiredArrival());
+      StopRequest sTwo = new StopRequest(r.getSite(),r.getDesiredDeparture());
+
+      List<StopRequest> expected = Arrays.asList(sOne,sTwo);
+
+      boolean testEq = actual.equals(expected);
+
+      assertEquals(true, testEq);
+
     }
 
-    @Test
-    public void testToStop(){
-    }
+    /* @Test */
+    /* public void testToStop(){ */
+    /*   Place p = new Place("Here"); */
+    /*   Time t = new Time(10,55); */
+    /*   StopRequest sr = new StopRequest(p,t); */
+    /*  */
+    /*   Stop expected = new Stop(p); */
+    /*   expected.setArrival(Time.addMinutes(desiredTime,-2)); */
+    /*   expected.setDeparture(Time.addMinutes(desiredTime,2)); */
+    /*  */
+    /*   Stop actual = sr.toStop(); */
+    /*  */
+    /* } */
 
     @Test
     public void testCompareTo(){
