7925a45e2c972c6c0da7e54dc8d7245a5d1747f8
ginozab <brandonginoza34@gmail.com>
2015-12-07 23:22:29 -0500

    created method calls for dailyrequest, finished grouping with tripgrouping.java

--- src/main/java/edu/allegheny/vanschedule/frontend/DailyRequests.java
+++ src/main/java/edu/allegheny/vanschedule/frontend/DailyRequests.java
@@ -4,6 +4,9 @@ import edu.allegheny.vanschedule.*;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 
 /*
 * @author Brandon Ginoza
@@ -26,19 +29,38 @@ public class DailyRequests {
 	 */
 
 	public static void makeDailyRequests(String[][][] allDayReqs) {
-
+		
+		System.out.println(printDate());
+		
 		// make a list of requests for each day of the week
 		ArrayList<Request> monday = DailyRequestMaker.getDailyRequests(allDayReqs[0]);
-		// List<Request> tuesday = DailyRequestMaker.getDailyRequests(allDayReqs[1]);
-		// List<Request> wednesday = DailyRequestMaker.getDailyRequests(allDayReqs[2]);
-		// List<Request> thursday = DailyRequestMaker.getDailyRequests(allDayReqs[3]);
-		// List<Request> friday = DailyRequestMaker.getDailyRequests(allDayReqs[4]);
+		ArrayList<Request> tuesday = DailyRequestMaker.getDailyRequests(allDayReqs[1]);
+		ArrayList<Request> wednesday = DailyRequestMaker.getDailyRequests(allDayReqs[2]);
+		ArrayList<Request> thursday = DailyRequestMaker.getDailyRequests(allDayReqs[3]);
+		ArrayList<Request> friday = DailyRequestMaker.getDailyRequests(allDayReqs[4]);
 
-		// call backend
+		// call backend for each day of the week
+		// it will return a route
 		Route mondayRoute = CodysScheduler.schedule(monday);
+		Route tuesdayRoute = CodysScheduler.schedule(tuesday);
+		Route wednesdayRoute = CodysScheduler.schedule(wednesday);
+		Route thursdayRoute = CodysScheduler.schedule(thursday);
+		Route fridayRoute = CodysScheduler.schedule(friday);
+
+		// call the remaining front end code
+		// this will add calendar events for each day route
 		TripGrouping.groupStops(mondayRoute);
-		//System.out.println(mondayRoute);
+		TripGrouping.groupStops(tuesdayRoute);
+		TripGrouping.groupStops(wednesdayRoute);
+		TripGrouping.groupStops(thursdayRoute);
+		TripGrouping.groupStops(fridayRoute);
 
 	} // makeDailyRequests
+	
+	public static String printDate() {
+	    Date date = new Date();
+	    SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
+	    return ft.format(date);
+	}
 
 }
--- src/main/java/edu/allegheny/vanschedule/frontend/TripGrouping.java
+++ src/main/java/edu/allegheny/vanschedule/frontend/TripGrouping.java
@@ -6,40 +6,59 @@ import java.util.ArrayList;
 
 public class TripGrouping {
 	
+	/*
+	 * @param Route
+	 * @return void
+	 * 
+	 * This method takes in a route and separates 
+	 * each trip by stops
+	 * Then calls CreateCalendarEvent class with new 
+	 * list of stops that are grouped by trip
+	 */
+	
 	public static void groupStops(Route r) {
 		
 		List<Stop> s = r.getStops();
 		
 		int counter = 0;
 		while(true) {
-			
+			// check to make sure we dont go out of bounds
 			if (counter == s.size()) {
 				break;
 			} // if size
 			
 			else {
-				
+				// find each allegheny stop so we can make new trip list
 				if (s.get(counter).getName().equals("Allegheny College")) {
 					List<Stop> tripSingle = new ArrayList<Stop>();
 					
+					// put in each stop for a trip
 					while(true) {
+						// add stop to trip
 						tripSingle.add(s.get(counter));
-						System.out.println(s.get(counter).getName());
+						//System.out.println(s.get(counter).getName());
 						counter++;
 						
+						// check counter position
 						if ((counter+1) == s.size()) {
 							counter++;
 							break;
 						}
 						
+						// if we found another allegheny stop that means there is 
+						// a new trip
 						if (s.get(counter).getName().equals("Allegheny College")) {
 							break;
 						} // inner if next is allegheny college
 						
 					} // inner while
+					
+					System.out.println("\n\n");
+					// call the createEvents() with each individual trip list
+					CreateCalendarEvents.createEvents(tripSingle);
 				} // if allegheny college
 			} // else
-				System.out.println();
+				//System.out.println();
 			
 		} // while
 		
